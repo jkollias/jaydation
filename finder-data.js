da515ac8f282659dbaa322153959bd15d42d1d19
jasonkollias <30324218+jasonkollias@users.noreply.github.com>
2021-09-21 23:15:09 -0400

    first commit

---
+++ finder-data.js
@@ -0,0 +1,133 @@
+// var finderData = {
+//     "shades": [
+//         {
+//             "skin_tone": "Fair",
+//             "options": [
+//                 {
+//                     "sku": "FC010",
+//                     "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC010",
+//                     "undertone": [
+//                         {
+//                             "cool": [
+//                                 {
+//                                     "sku": "FC010",
+//                                     "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC010"
+//                                 }
+//                             ],
+//                             "neutral": [
+//                                 {
+//                                     "sku": "FN020",
+//                                     "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FN020"
+//                                 },
+//                                 {
+//                                     "sku": "FN030",
+//                                     "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FN030"
+//                                 }
+//                             ]
+//                         }
+//                     ]
+//                 },
+//                 {
+//                     "sku": "FC050",
+//                     "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC050",
+//                     "undertone": [
+//                         {
+//                             "cool": [
+//                                 {
+//                                     "sku": "FN030",
+//                                     "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FN030"
+//                                 },
+//                                 {
+//                                     "sku": "FC040",
+//                                     "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC040"
+//                                 }
+//                             ],
+//                             "neutral": [
+//                                 {
+//                                     "sku": "FC050",
+//                                     "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC050"
+//                                 }
+//                             ]
+//                         }
+//                     ]
+//                 }
+//             ],
+//         },
+//         {
+//             "skin_tone": "Light",
+//         },
+//         {
+//             "skin_tone": "Medium"
+//         },
+//         {
+//             "skin_tone": "Deep"
+//         }
+//     ]
+// }
+var finderData = {
+    "shades": [
+        {
+            "skin_tone": "Fair",
+            "options": [
+                {
+                    //"sku": "FC010",
+                    "sku": "123", 
+                    "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC010",
+                    "undertone": [
+                        {
+                            "cool": [
+                                {
+                                    "sku": "FC010",
+                                    "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC010"
+                                }
+                            ],
+                            "neutral": [
+                                {
+                                    "sku": "FN020",
+                                    "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FN020"
+                                },
+                                {
+                                    "sku": "FN030",
+                                    "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FN030"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "sku": "FC050",
+                    "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC050",
+                    "undertone": [
+                        {
+                            "cool": [
+                                {
+                                    "sku": "FN030",
+                                    "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FN030"
+                                },
+                                {
+                                    "sku": "FC040",
+                                    "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC040"
+                                }
+                            ],
+                            "neutral": [
+                                {
+                                    "sku": "FC050",
+                                    "image": "https://via.placeholder.com/500x800.png?text=Step+1:+FC050"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+        },
+        {
+            "skin_tone": "Light",
+        },
+        {
+            "skin_tone": "Medium"
+        },
+        {
+            "skin_tone": "Deep"
+        }
+    ]
+}
